007bc4f3e40540027a547b05e712facd187c0631
sahil <sahiltyagi1204@outlook.com>
2023-05-15 16:57:06 +0530

    article got created through post request and saved in DB

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const ejs = require('ejs')
+const bodyParser = require('body-parser')
 
 const app = express()
 
 app.set('view engine' , 'ejs')
 app.use(express.static("public"))
+
+// const reqParser = bodyParser.json()
+app.use(bodyParser.urlencoded({extended :true}))
+
+// SETTING UP DATABASE
 
 mongoose.connect("mongodb://127.0.0.1/wikiDB")
 .then(()=>{
@@ -13,6 +19,49 @@ mongoose.connect("mongodb://127.0.0.1/wikiDB")
 }).catch((err)=>{
 console.log(err);
 })
+
+const blogSchema = mongoose.Schema({
+    title : String,
+    content : String,
+})
+
+const blogMODEL = mongoose.model('blogModel' , blogSchema)
+
+
+// ADDING ROUTES
+
+app.get('/' , (req , res)=>{
+    res.send("this is the home route , please visit /articles route to get to the articles page ")
+})
+
+
+app.get('/articles' ,(req , res)=>{
+    blogMODEL.find().then((foundArticle)=>{
+        // console.log(foundArticle);
+    res.send(foundArticle)
+
+    }).catch((err)=>{
+        res.send(err)
+    })
+   })
+
+   app.post('/articles' , (req ,res)=>{
+    console.log(req.body.title)
+    console.log(req.body.content)
+
+    const newArticle = new blogMODEL({
+        title : req.body.title,
+        content : req.body.content
+
+    })
+
+    newArticle.save().then(()=>{
+        res.send("new article has been created")
+    }).catch((err)=>{
+        console.log(err);
+    })
+
+   })
 
 
 
